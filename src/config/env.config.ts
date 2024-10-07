3441166284c51100db1956c2a6dd41237e5733a4
HoaTran1002 <vanhoa0908751520@gmail.com>
2024-10-07 18:03:04 +0700

    setup(config): update config error

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,6 +1,21 @@
 import "dotenv/config";
-
+import { response } from "express";
+import {z} from 'zod'
 const envs = process.env
+const envSchema = z.object({
+    PORT:z.number(),
+    MYSQL_DATABASE:z.string(),
+    MYSQL_USER:z.string(),
+    MYSQL_PASSWORD:z.string(),
+    MYSQL_ROOT_PASSWORD:z.string()
+})
+try {
+    const resultValidate = envSchema.parse(envs)
+
+} catch (error) {
+    // throw new Error(error)
+}
+
 export const varsEnv = {
     PORT:envs.PORT,
     MYSQL_DATABASE:envs.MYSQL_DATABASE,
